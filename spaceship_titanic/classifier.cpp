3ad1ebb6d8299bd4d1cd8fc3f9c51c9d1a38d06e
hatemalamir <hatem.alamir.90@gmail.com>
2024-11-11 07:32:28 -0500

    spaceship_titanic: add Random Forest Classifier

--- spaceship_titanic/classifier.cpp
+++ spaceship_titanic/classifier.cpp
@@ -10,13 +10,8 @@ int main()
     arma::Row<size_t> labels;
 
     mlpack::data::Load("train_data.csv", dataset, info, true);
-    std::cout << ">> Loaded training data, "
-              << dataset.n_cols << " observations, "
-              << info.Dimensionality() << " features." << std::endl;
 
     mlpack::data::Load("train_labels.csv", labels);
-    std::cout << ">> Loaded training labels, "
-              << labels.size() << " labels, " << std::endl;
 
     arma::mat trainDataset, validDataset;
     arma::Row<size_t> trainLabels, validLabels;
@@ -26,14 +21,27 @@ int main()
               << validDataset.n_cols << " validation." << std::endl;
 
     std::cout << ">> Training a RandomForest classifier..." << std::endl;
-    mlpack::RandomForest<> r(
-        trainDataset,
-        trainLabels,
-        7 /*Classes*/,
-        10 /*Trees*/,
-        3 /*Minimum leaf size*/
-    );
-
+    std::vector<double> rfNTree{1, 10, 50, 100, 150, 200, 300, 500};
+    std::vector<double> rfTrainScores, rfValidScores;
+    for(const auto& nTree: rfNTree) {
+        mlpack::RandomForest<> rfClf(
+            trainDataset,
+            info,
+            trainLabels,
+            2 /*Classes*/,
+            nTree /*Trees*/
+        );
+        arma::Row<size_t> rfTrainPreds, rfValidPreds;
+        rfClf.Classify(trainDataset, rfTrainPreds);
+        rfClf.Classify(validDataset, rfValidPreds);
+        const double rfTrainScore = arma::accu(rfTrainPreds == trainLabels) * 100.0 / trainLabels.n_elem;
+        rfTrainScores.push_back(rfTrainScore);
+        const double rfValidScore = arma::accu(rfValidPreds == validLabels) * 100.0 / validLabels.n_elem;
+        rfTrainScores.push_back(rfValidScore);
+        std::cout << ".. Trees: " << nTree
+                  << ", Train Score: " << rfTrainScore
+                  << "%, Validation Score: " << rfValidScore << "%" << std::endl;
+    }
 
     std::cout << std::endl << "Done!";
     return 0;
